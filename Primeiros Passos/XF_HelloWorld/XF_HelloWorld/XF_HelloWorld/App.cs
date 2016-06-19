d9d2db85848f4c9809e6554fc608e4c411bba2c9
Paulo Roberto Ramos de Andrade <paulo.roberto@randrade.net>
2016-06-19 16:38:52 -0300

    Adição de código fonte
    Projeto de HelloWorld em Xamarin.Forms

---
+++ Primeiros Passos/XF_HelloWorld/XF_HelloWorld/XF_HelloWorld/App.cs
@@ -0,0 +1,35 @@
+﻿using Xamarin.Forms;
+
+namespace XF_HelloWorld
+{
+    public class App : Application
+    {
+        public App()
+        {
+            MainPage = new ContentPage
+            {
+                Content = new Label
+                {
+                    Text = "Hello World!",
+                    VerticalOptions = LayoutOptions.Center,
+                    HorizontalOptions = LayoutOptions.Center
+                }
+            };
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
