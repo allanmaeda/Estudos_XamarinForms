9c2215ebf9a9c5e6f69caae5fc8c7f3e84951587
Paulo Roberto Ramos de Andrade <paulo.roberto@randrade.net>
2016-08-07 11:45:50 -0300

    Adição de projeto de interface com Xaml

---
+++ Layouts/Geral/XF_Layout_MultiplasViews_Xaml/XF_Layout_MultiplasViews_Xaml/XF_Layout_MultiplasViews_Xaml/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace XF_Layout_MultiplasViews_Xaml
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_OnClicked(object sender, EventArgs e)
+        {
+            Texto.Text = string.IsNullOrEmpty(CaixaDeTexto.Text)
+                    ? "Você precisa digitar um texto!"
+                    : CaixaDeTexto.Text;
+        }
+    }
+}
