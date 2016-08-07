d503db9431a063ecd556172aafb937601908c02f
Paulo Roberto Ramos de Andrade <paulo.roberto@randrade.net>
2016-08-06 22:42:25 -0300

    Adição de projeto de Layouts para Xamarin.Forms

---
+++ Layouts/Geral/XF_Layout_MultiplasViews_Code/XF_Layout_MultiplasViews_Code/XF_Layout_MultiplasViews_Code/MainPage.cs
@@ -0,0 +1,40 @@
+﻿using Xamarin.Forms;
+
+namespace XF_Layout_MultiplasViews_Code
+{
+    public class MainPage : ContentPage
+    {
+        private Button Botao { get; }
+
+        private Entry CaixaDeTexto { get; }
+
+        private Label Texto { get; }
+
+        public MainPage()
+        {
+            Botao = new Button { Text = "Clique aqui! "};
+            CaixaDeTexto = new Entry
+            {
+                HorizontalOptions = LayoutOptions.Fill
+            };
+            Texto = new Label { Text = "Texto exemplo" };
+            Botao.Clicked += (sender, args) =>
+            {
+                Texto.Text = string.IsNullOrEmpty(CaixaDeTexto.Text) 
+                    ? "Você precisa digitar um texto!"
+                    : CaixaDeTexto.Text;
+            };
+
+            Content = new StackLayout
+            {
+                VerticalOptions = LayoutOptions.Center,
+                Children = {
+                    Texto,
+                    Botao,
+                    CaixaDeTexto
+                }
+            };
+        }
+    }
+}
+
