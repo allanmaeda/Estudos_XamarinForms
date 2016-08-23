c0c4d5bcd4c0d644e87a8f179c204573812a4566
Paulo Roberto Ramos de Andrade <paulo.roberto@randrade.net>
2016-08-22 22:02:35 -0300

    Adição do projeto XF_StackLayout_ExemploPratico

---
+++ Primeiros Passos/XF_StackLayout_ExemploPratico/XF_StackLayout_ExemploPratico/XF_StackLayout_ExemploPratico/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Xamarin.Forms;
+
+namespace XF_StackLayout_ExemploPratico
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Slider_OnValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            var color = Color.FromRgb(SliderR.Value, SliderG.Value, SliderB.Value);
+            Caixa.BackgroundColor = color;
+        }
+    }
+}
