1a05695244277a7de4eea6f71a5e46b6dc6279b1
Paulo Roberto Ramos de Andrade <paulo.roberto@randrade.net>
2016-09-03 22:56:24 -0300

    Novo projeto sobre ScrollView

---
+++ Layouts/ScrollView/XF_ScrollView_Parte1/XF_ScrollView_Parte1/XF_ScrollView_Parte1/MainPage.cs
@@ -0,0 +1,41 @@
+﻿using Xamarin.Forms;
+
+namespace XF_ScrollView_Parte1
+{
+    public class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            Content = new ScrollView
+            {
+                Content = new StackLayout
+                {
+                    Children =
+                    {
+                        new Label {Text = "Campo 1"},
+                        new Entry(),
+                        new Label {Text = "Campo 2"},
+                        new Entry(),
+                        new Label {Text = "Campo 3"},
+                        new Entry(),
+                        new Label {Text = "Campo 4"},
+                        new Entry(),
+                        new Label {Text = "Campo 5"},
+                        new Entry(),
+                        new Label {Text = "Campo 6"},
+                        new Entry(),
+                        new Label {Text = "Campo 7"},
+                        new Entry(),
+                        new Label {Text = "Campo 8"},
+                        new Entry(),
+                        new Label {Text = "Campo 9"},
+                        new Entry(),
+                        new Label {Text = "Campo 10"},
+                        new Entry()
+                    }
+                }
+            };
+        }
+    }
+}
+
