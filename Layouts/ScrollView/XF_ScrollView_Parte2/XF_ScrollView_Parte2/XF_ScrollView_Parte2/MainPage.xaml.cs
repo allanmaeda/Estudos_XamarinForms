bae3835eb3e1bb642382a81cd0bfea03936c0bee
Paulo Roberto Ramos de Andrade <paulo.roberto@randrade.net>
2016-09-10 10:47:18 -0300

    Adição de novo projeto sobre o funcionamento de um ScrollView no Xamarin.Forms

---
+++ Layouts/ScrollView/XF_ScrollView_Parte2/XF_ScrollView_Parte2/XF_ScrollView_Parte2/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace XF_ScrollView_Parte2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
