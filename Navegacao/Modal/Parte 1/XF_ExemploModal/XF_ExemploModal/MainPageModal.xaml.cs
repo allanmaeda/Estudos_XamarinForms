6dc9e5f506130892c55e2ac213d320cc90b66a9e
Paulo Roberto Ramos de Andrade <paulo.roberto@randrade.net>
2016-09-24 18:50:18 -0300

    Adicionando primeiro projeto de navegação via Modals

---
+++ Navegacao/Modal/Parte 1/XF_ExemploModal/XF_ExemploModal/MainPageModal.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace XF_ExemploModal
+{
+    public partial class MainPageModal : ContentPage
+    {
+        public MainPageModal()
+        {
+            InitializeComponent();
+        }
+
+        private async void ButtonFecharModal_OnClicked(object sender, EventArgs e)
+        {
+            await Navigation.PopModalAsync(true);
+        }
+    }
+}
