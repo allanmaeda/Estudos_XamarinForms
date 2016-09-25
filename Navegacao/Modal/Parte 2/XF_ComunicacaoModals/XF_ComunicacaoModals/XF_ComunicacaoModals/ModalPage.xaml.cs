3de6b8975387585df23a6335fa3ab1e31c1f1db6
Paulo Roberto Ramos de Andrade <paulo.roberto@randrade.net>
2016-09-25 16:53:05 -0300

    Adição de projeto de navegação entre Pages com Modal

---
+++ Navegacao/Modal/Parte 2/XF_ComunicacaoModals/XF_ComunicacaoModals/XF_ComunicacaoModals/ModalPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace XF_ComunicacaoModals
+{
+    public partial class ModalPage : ContentPage
+    {
+        public ModalPage(string informacao)
+        {
+            InitializeComponent();
+            Label.Text = informacao;
+        }
+
+        private async void FecharModal_OnClicked(object sender, EventArgs e)
+        {
+            await Navigation.PopModalAsync(true);
+        }
+    }
+}
