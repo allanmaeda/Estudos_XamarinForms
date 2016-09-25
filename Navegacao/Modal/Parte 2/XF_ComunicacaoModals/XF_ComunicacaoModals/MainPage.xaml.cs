d7d04aeafc85c91c2f830091e1ea852bec8fc31c
Paulo Roberto Ramos de Andrade <paulo.roberto@randrade.net>
2016-09-25 16:57:22 -0300

    ...

---
+++ Navegacao/Modal/Parte 2/XF_ComunicacaoModals/XF_ComunicacaoModals/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace XF_ComunicacaoModals
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void BtnAbrirModal_OnClicked(object sender, EventArgs e)
+        {
+            var modalPage1 = new ModalPage(Entry.Text);
+            await Navigation.PushModalAsync(modalPage1, true);
+        }
+    }
+}
