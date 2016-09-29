5d4e05681966654f849c94d7490765c803bb322c
Paulo Roberto Ramos de Andrade <paulo.roberto@randrade.net>
2016-09-29 20:52:32 -0300

    Adição de projeto que mostra como usar eventos na comunicação entre uma ModalPage e sua Page Principal

---
+++ Navegacao/Modal/Parte 4/XF_ComunicacaoModalEventos/XF_ComunicacaoModalEventos/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace XF_ComunicacaoModalEventos
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_OnClicked(object sender, EventArgs e)
+        {
+            // criando a modalPage e amarrando um método ao ModalHandler
+            var modalPage = new ModalPage
+            {
+                ModalHandler = (o, args) =>
+                {
+                    Entry.Text = (string)o;
+                }
+            };
+
+            await Navigation.PushModalAsync(modalPage, true);
+        }
+    }
+}
---
+++ Navegacao/Modal/Parte 4/XF_ComunicacaoModalEventos/XF_ComunicacaoModalEventos/ModalPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace XF_ComunicacaoModalEventos
+{
+    public partial class ModalPage : ContentPage
+    {
+        // criando event handler para amarrar funções externas
+        public EventHandler ModalHandler { get; set; }
+
+        public ModalPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_OnClicked(object sender, EventArgs e)
+        {
+            // se o event handler tiver alguma função, chame-a
+            ModalHandler?.Invoke(Entry.Text, EventArgs.Empty);
+            await Navigation.PopModalAsync(true);
+        }
+    }
+}
