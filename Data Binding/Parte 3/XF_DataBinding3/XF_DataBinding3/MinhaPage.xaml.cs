8c2d7b3fc58f8fcf48fe2f759f68e88492d5a82c
Paulo Roberto Ramos de Andrade <paulo.roberto@randrade.net>
2016-10-23 15:50:26 -0200

    Adicionando projeto de Data Binding Parte 3

---
+++ Data Binding/Parte 3/XF_DataBinding3/XF_DataBinding3/MinhaPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Diagnostics;
+using Xamarin.Forms;
+
+namespace XF_DataBinding3
+{
+    public partial class MinhaPage : ContentPage
+    {
+        public Model Model { get; }
+
+        public MinhaPage()
+        {
+            InitializeComponent();
+
+            Model = new Model
+            {
+                Nome = "Usuario exemplo",
+                Idade = 20
+            };
+
+            BindingContext = Model;
+        }
+
+        private void ButtonInformacoes_OnClicked(object sender, EventArgs e)
+        {
+            Debug.WriteLine($"Model.Nome: {Model.Nome}");
+            Debug.WriteLine($"Model.Idade: {Model.Idade}"); 
+        }
+
+        private void ButtonAleatorio_OnClicked(object sender, EventArgs e)
+        {
+            Model.Nome = "usuário teste";
+            Model.Idade = new Random().Next(10, 50);
+        }
+    }
+}
---
+++ Data Binding/Parte 3/XF_DataBinding3/XF_DataBinding3/Model.cs
@@ -0,0 +1,9 @@
+﻿namespace XF_DataBinding3
+{
+    public class Model
+    {
+        public string Nome { get; set; }
+
+        public int Idade { get; set; }
+    }
+}
