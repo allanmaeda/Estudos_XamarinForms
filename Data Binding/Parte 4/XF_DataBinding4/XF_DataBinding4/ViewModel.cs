7f2b7fc7a7d95f2296ca26d3a41a81e21f7e8d5e
Paulo <Paulo@DESKTOP-HOTO6IV>
2016-11-06 16:20:55 -0200

    Novo projeto de Data Binding XF_DataBinding4 mostrando os primeiros passos do padrão MVVM

---
+++ Data Binding/Parte 4/XF_DataBinding4/XF_DataBinding4/ViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace XF_DataBinding4
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private DateTime _dataAtual;
+        public DateTime DataAtual
+        {
+            set
+            {
+                if (_dataAtual == value)
+                    return;
+
+                _dataAtual = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(DataAtual)));
+            }
+            get
+            {
+                return _dataAtual;
+            }
+        }
+
+        public ViewModel()
+        {
+            DataAtual = DateTime.Now;
+
+            Device.StartTimer(TimeSpan.FromSeconds(1), () =>
+            {
+                DataAtual = DateTime.Now;
+                return true;
+            });
+        }
+    }
+}
