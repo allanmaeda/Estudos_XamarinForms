44e92170a84b9e8c5e36520f99c833a38e7e2650
Paulo <Paulo@DESKTOP-HOTO6IV>
2016-11-09 05:15:52 -0200

    Adição de novo projeto de Data Binding, usando MVVM

---
+++ Data Binding/Parte 5/XF_DataBinding5/XF_DataBinding5/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics;
+using Xamarin.Forms;
+
+namespace XF_DataBinding5
+{
+    public partial class MainPage : ContentPage
+    {
+        public ViewModel ViewModel { get; set; }
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            ViewModel = new ViewModel();
+            BindingContext = ViewModel;
+        }
+
+        private void ImprimirInformacoes(object sender, EventArgs e)
+        {
+            Debug.WriteLine($"Nome: {ViewModel.Nome}, Sobrenome: {ViewModel.Sobrenome}");    
+        }
+
+        private void ForcarInformacaoViaBackend(object sender, EventArgs e)
+        {
+            ViewModel.Nome = "Nome teste";
+            ViewModel.Sobrenome = "Sobrenome teste";
+        }
+    }
+}
---
+++ Data Binding/Parte 5/XF_DataBinding5/XF_DataBinding5/ViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel;
+
+namespace XF_DataBinding5
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string _nome;
+        private string _sobrenome;
+
+        public string Nome
+        {
+            get { return _nome; }
+            set
+            {
+                _nome = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Nome)));
+            }
+        }
+
+        public string Sobrenome
+        {
+            get { return _sobrenome; }
+            set
+            {
+                _sobrenome = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Sobrenome)));
+            }
+        }
+    }
+}
