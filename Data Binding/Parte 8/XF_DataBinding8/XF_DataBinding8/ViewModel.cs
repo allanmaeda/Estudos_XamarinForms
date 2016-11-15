3723d5bce02578e17cbf1fce1c6f17b3f900acf6
Paulo <Paulo@DESKTOP-HOTO6IV>
2016-11-15 17:32:41 -0200

    Adição de novo projeto MVVM para Xamarin.Forms

---
+++ Data Binding/Parte 8/XF_DataBinding8/XF_DataBinding8/ViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace XF_DataBinding8
+{
+    public class ViewModel
+    {
+        public ICommand ModificaTexto { get; }
+
+        public ViewModel()
+        {
+            ModificaTexto = new Command<string>(s =>
+            {
+                if (!string.IsNullOrEmpty(s))
+                    Debug.WriteLine($"Texto modificado: {s.ToUpper()}");
+            });
+        }
+    }
+
+
+}
