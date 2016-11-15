11aa0254331ce5c5689207988861e45f30e65b89
Paulo <Paulo@DESKTOP-HOTO6IV>
2016-11-15 14:53:45 -0200

    Adição de novo projeto de MVVM para Xamarin.Forms

---
+++ Data Binding/Parte 7/XF_DataBinding7/XF_DataBinding7/ViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace XF_DataBinding7
+{
+    public class ViewModel
+    {
+        public ICommand Click { get; }
+
+        public void BotaoClicado()
+        {
+            Debug.WriteLine("funcao chamada");
+        }
+
+        public ViewModel()
+        {
+            Click = new Command(BotaoClicado);
+        }
+    }
+}
