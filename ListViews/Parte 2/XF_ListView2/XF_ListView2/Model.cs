dee58bd622fd9a6fe4d3cb4d1e63318e390e01f8
Paulo <Paulo@DESKTOP-HOTO6IV>
2016-12-03 21:56:17 -0200

    Adicionando novo projeto de ListViews para Xamarin.Forms

---
+++ ListViews/Parte 2/XF_ListView2/XF_ListView2/Model.cs
@@ -0,0 +1,11 @@
+﻿namespace XF_ListView2
+{
+    public class Model
+    {
+        public string Nome { get; set; }
+
+        public string Sobrenome { get; set; }
+
+        public int Idade { get; set; }
+    }
+}
