c503061d7e618a6504f1d6cd3817efbf6515a741
Paulo <Paulo@DESKTOP-HOTO6IV>
2017-01-14 21:21:02 -0200

    Novo projeto de ListView para Xamarin.Forms

---
+++ ListViews/Parte 7/XF_ListView7/XF_ListView7/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using Xamarin.Forms;
+
+namespace XF_ListView7
+{
+    public partial class MainPage : ContentPage
+    {
+        private ObservableCollection<Model> Lista { get; }
+        private int _i;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            Lista = new ObservableCollection<Model>();
+            MyListView.ItemsSource = Lista;
+        }
+
+        private void Button_OnClicked(object sender, EventArgs e)
+        {
+            Lista.Insert(0, new Model($"principal {++_i}", $"secundaria {_i}"));
+        }
+
+        private void MyListView_OnItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var itemSelecionado1 = (Model) e.SelectedItem;
+            var itemSelecionado2 = (Model) MyListView.SelectedItem;
+
+            // item vindo pelo SelectedItemChangedEventArgs
+            Debug.WriteLine($"Hora: {itemSelecionado1.Hora}");
+            Debug.WriteLine($"Info1: {itemSelecionado1.InformacaoPrincipal}");
+            Debug.WriteLine($"Info2: {itemSelecionado1.InformacaoSecundaria}");
+            Debug.WriteLine("------");
+
+            // item vindo da listview
+            Debug.WriteLine($"Hora: {itemSelecionado2.Hora}");
+            Debug.WriteLine($"Info1: {itemSelecionado2.InformacaoPrincipal}");
+            Debug.WriteLine($"Info2: {itemSelecionado2.InformacaoSecundaria}");
+            Debug.WriteLine("------");
+        }
+    }
+}
---
+++ ListViews/Parte 7/XF_ListView7/XF_ListView7/Model.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace XF_ListView7
+{
+    public class Model
+    {
+        public string InformacaoPrincipal { get; }
+
+        public string InformacaoSecundaria { get; }
+
+        public string Hora { get; }
+
+        public Model(string principal, string secundaria)
+        {
+            InformacaoPrincipal = principal;
+            InformacaoSecundaria = secundaria;
+            Hora = DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss.FFF");
+        }
+    }
+}
