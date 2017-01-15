0296774bc6e7edc3b3c6cfc6ac9c6151b778b2bd
Paulo <Paulo@DESKTOP-HOTO6IV>
2017-01-14 23:10:25 -0200

    Novo projeto de ListView para Xamarin.Forms

---
+++ ListViews/Parte 8/XF_ListView8/XF_ListView8/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace XF_ListView8
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            var list = new List<Model>();
+
+            for (var i = 1; i<= 20 ; i++)
+                list.Add(new Model { Informacao = $"Item {i:D2}" });
+
+            MyListView.ItemsSource = list;
+        }
+
+        private void MyListView_OnItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            MyListView.SelectedItem = null;
+        }
+    }
+}
