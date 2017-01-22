dc1323152677bf57319fe21c1f5e0453a93a6ed0
Paulo <Paulo@DESKTOP-HOTO6IV>
2017-01-22 15:07:35 -0200

    Adição de novo projeto de ListView para Xamarin.Forms

---
+++ ListViews/Parte 12/XF_ListView12/XF_ListView12/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using Xamarin.Forms;
+
+namespace XF_ListView12
+{
+    public partial class MainPage : ContentPage
+    {
+        public ObservableCollection<Model> Lista { get; }
+
+        public MainPage()
+        {
+            InitializeComponent();
+            Lista = new ObservableCollection<Model>();
+
+            for (var i = 1; i <= 30; i++)
+            {
+                Lista.Add(new Model
+                {
+                    Informacao = $"Elemento {i:D2}"
+                });
+            }
+
+            MyListView.ItemsSource = Lista;
+        }
+
+        private void MenuItemMais_OnClicked(object sender, EventArgs e)
+        {
+            var mi = (MenuItem)sender;
+            var model = (Model)mi.CommandParameter;
+            Label.Text = $"Celula com texto {model.Informacao} foi escolhida";
+        }
+
+        private void MenuItemApagar_OnClicked(object sender, EventArgs e)
+        {
+            var mi = (MenuItem) sender;
+            var model = (Model)mi.CommandParameter;
+            Lista.Remove(model);
+        }
+    }
+}
