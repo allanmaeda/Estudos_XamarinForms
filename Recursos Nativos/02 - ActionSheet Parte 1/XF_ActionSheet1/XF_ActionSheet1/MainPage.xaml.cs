49205bf9019c4073bb190e8e2d35bc6f75c581a9
Paulo <Paulo@DESKTOP-HOTO6IV>
2017-01-28 17:57:37 -0200

    Adição de projeto sobre ActionSheet para Xamarin.Forms

---
+++ Recursos Nativos/02 - ActionSheet Parte 1/XF_ActionSheet1/XF_ActionSheet1/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XF_ActionSheet1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_OnClicked(object sender, EventArgs e)
+        {
+            var result = await DisplayActionSheet("Titulo", null, null, "opção 1", "opção 2", "opção 3");
+            Label.Text = result;
+            await Task.Delay(1000);
+            Label.Text = "";
+        }
+    }
+}
