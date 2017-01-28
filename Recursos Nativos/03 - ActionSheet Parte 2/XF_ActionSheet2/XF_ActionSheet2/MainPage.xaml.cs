ceb55a4c7b5c87602bc1108a980725062854515c
Paulo <Paulo@DESKTOP-HOTO6IV>
2017-01-28 18:47:31 -0200

    Adição de novo projeto sobre ActionSheet para Xamarin.Forms

---
+++ Recursos Nativos/03 - ActionSheet Parte 2/XF_ActionSheet2/XF_ActionSheet2/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace XF_ActionSheet2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_OnClicked1(object sender, EventArgs e)
+        {
+            var result = await DisplayActionSheet("Título", "Botão cancelar", null, "opção 1", "opção 2");
+            Label.Text = $"Primeiro DisplayAlert fechado com {result}";
+        }
+
+        private async void Button_OnClicked2(object sender, EventArgs e)
+        {
+            var result = await DisplayActionSheet("Título", null, "Botão destruir", "opção 1", "opção 2");
+            Label.Text = $"Segundo DisplayAlert fechado com {result}";
+        }
+
+        private async void Button_OnClicked3(object sender, EventArgs e)
+        {
+            var result = await DisplayActionSheet("Título", "Botão cancelar", "Botão destruir", "opção 1", "opção 2");
+            Label.Text = $"Terceiro DisplayAlert fechado com {result}";
+        }
+    }
+}
