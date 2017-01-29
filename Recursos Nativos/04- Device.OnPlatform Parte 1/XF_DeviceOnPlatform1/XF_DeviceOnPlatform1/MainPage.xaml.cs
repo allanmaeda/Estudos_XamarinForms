58bec84ae4b535c0d3db3ea0a0b6fee9818db763
Paulo <Paulo@DESKTOP-HOTO6IV>
2017-01-29 18:44:04 -0200

    Adicionando novo projeto sobre a classe Device.OnPlatform para Xamarin.Forms

---
+++ Recursos Nativos/04- Device.OnPlatform Parte 1/XF_DeviceOnPlatform1/XF_DeviceOnPlatform1/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace XF_DeviceOnPlatform1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void ButtonExecutarAcao_OnClicked(object sender, EventArgs e)
+        {
+            var result = string.Empty;
+            Action acaoIos = () => result = "Método rodou no iOS";
+            Action acaoAndroid = () => result = "Método rodou no Android";
+            Action acaoPadrao = () => result = "Método padrão";
+            Device.OnPlatform(acaoIos, acaoAndroid, null, acaoPadrao);
+            await DisplayAlert("Atenção", result, "OK");
+        }
+
+        private async void ButtonRetornarValor_OnClicked(object sender, EventArgs e)
+        {
+            var result = Device.OnPlatform(
+                "String retornado no iOS",
+                "String retornado no Android",
+                "String retornado no WinPhone");
+            await DisplayAlert("Atenção", result, "OK");
+        }
+    }
+}
