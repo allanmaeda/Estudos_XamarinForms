652c59462b40ec268ec9914bf66840fc8decb96f
Paulo <Paulo@DESKTOP-HOTO6IV>
2017-01-29 19:18:30 -0200

    Adição de novo projeto sobre Device.OnPlatform para Xamarin.Forms

---
+++ Recursos Nativos/05 - Device.OnPlatform Parte 2/XF_DeviceOnPlatform2/XF_DeviceOnPlatform2/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+
+namespace XF_DeviceOnPlatform2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            var valor = Device.OnPlatform(20, 0, 0);
+            Padding = new Thickness(0, valor, 0, 0);
+        }
+    }
+}
