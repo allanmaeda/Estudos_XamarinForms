6185799c263f9dac689060a250dfa8f537ff5cd3
Paulo <Paulo@DESKTOP-HOTO6IV>
2017-02-04 20:34:39 -0200

    Projeto sobre as propriedades Device.OS e Device.Idiom para Xamarin.Forms

---
+++ Recursos Nativos/08 - Device.OS e Device.Idiom/XF_DeviceEnums/XF_DeviceEnums/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace XF_DeviceEnums
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void PrimeiroBotao_OnClicked(object sender, EventArgs e)
+        {
+            if (Device.OS == TargetPlatform.Android)
+                await DisplayAlert("Atenção", "Código está rodando em Android", "Ok");
+            else if (Device.OS == TargetPlatform.iOS)
+                await DisplayAlert("Atenção", "Código está rodando em iOS", "Ok");
+        }
+
+        private async void SegundoBotao_OnClicked(object sender, EventArgs e)
+        {
+            if (Device.Idiom == TargetIdiom.Desktop)
+                await DisplayAlert("Atenção", "Código está rodando em desktop", "Ok");
+            if (Device.Idiom == TargetIdiom.Phone)
+                await DisplayAlert("Atenção", "Código está rodando em smartphone", "Ok");
+            if (Device.Idiom == TargetIdiom.Tablet)
+                await DisplayAlert("Atenção", "Código está rodando em tablet", "Ok");
+        }
+    }
+}
