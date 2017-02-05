b728e60890796ebb8c92e100ef5075f61765a9dd
Paulo <Paulo@DESKTOP-HOTO6IV>
2017-02-05 15:34:58 -0200

    Adicionando projeto sobre Device.StartTimer no Xamarin.Forms

---
+++ Recursos Nativos/09 - Device.StartTimer/XF_DeviceTimer/XF_DeviceTimer/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace XF_DeviceTimer
+{
+    public partial class MainPage : ContentPage
+    {
+        private int _vezesTimer;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_OnClicked(object sender, EventArgs e)
+        {
+            Device.StartTimer(TimeSpan.FromSeconds(1), () =>
+            {
+                if (_vezesTimer == 4)
+                {
+                    _vezesTimer = 0;
+                    Button.IsEnabled = true;
+                    Entry.Text = string.Empty;
+                    return false;
+                }
+
+                Entry.Text = $"Timer foi executado {++_vezesTimer} vezes";
+                Button.IsEnabled = false;
+                return true;
+            });
+        }
+    }
+}
