8a563a68f369ab808229a5c7205434d7e6b223fe
Paulo <Paulo@DESKTOP-HOTO6IV>
2017-02-18 20:51:48 -0200

    Adição de novo projeto de abstração de Xamarin.Forms, usando Device.BeginInvokeOnMainThread

---
+++ Recursos Nativos/10 - Device.BeginInvokeOnMainThread/XF_BeginInvokeOnMainThread/XF_BeginInvokeOnMainThread/XF_BeginInvokeOnMainThread/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XF_BeginInvokeOnMainThread
+{
+    public partial class MainPage : ContentPage
+    {
+        private readonly Random r;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            r = new Random();
+        }
+
+        private async void Button_OnClicked(object sender, EventArgs e)
+        {
+            await Task.Factory.StartNew(() =>
+            {
+                var valorSorteado = r.Next(0, 10);
+                Device.BeginInvokeOnMainThread(() =>
+                {
+                    Button.Text = $"O valor sorteado foi {valorSorteado}";
+                });
+            });
+        }
+    }
+}
