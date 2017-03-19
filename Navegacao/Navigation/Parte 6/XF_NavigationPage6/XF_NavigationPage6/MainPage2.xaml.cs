6f3a39ed907e2b9287517cdc383378f1c43b7b23
Paulo <Paulo@DESKTOP-HOTO6IV>
2017-03-19 13:20:23 -0300

    Adição de projeto sobre Navigation para Xamarin.Forms

---
+++ Navegacao/Navigation/Parte 6/XF_NavigationPage6/XF_NavigationPage6/MainPage2.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XF_NavigationPage6
+{
+    public partial class MainPage2 : ContentPage
+    {
+        public MainPage2()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_OnClicked(object sender, EventArgs e)
+        {
+            await Navigation.PopAsync();
+        }
+    }
+}
