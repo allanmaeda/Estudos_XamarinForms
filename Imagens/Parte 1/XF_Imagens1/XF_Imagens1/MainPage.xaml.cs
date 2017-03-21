77d38688df6a6df17a8ded38cde0a7958b50bfd2
Paulo <Paulo@DESKTOP-HOTO6IV>
2017-03-21 20:43:07 -0300

    Adição de novo projeto sobre inserção de imagens no Xamarin.Forms

---
+++ Imagens/Parte 1/XF_Imagens1/XF_Imagens1/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Xamarin.Forms;
+
+namespace XF_Imagens1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            var image = new Image
+            {
+                Source = ImageSource.FromResource("XF_Imagens1.imagem.png"),
+                VerticalOptions = LayoutOptions.Center,
+                HorizontalOptions = LayoutOptions.Center
+            };
+
+            Content = image;
+        }
+    }
+}
