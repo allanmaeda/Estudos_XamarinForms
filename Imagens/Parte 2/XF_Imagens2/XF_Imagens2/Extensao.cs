f64ad06716f3f65d32e5851543615b4841ffa05c
Paulo <Paulo@DESKTOP-HOTO6IV>
2017-03-21 21:47:50 -0300

    Adição de novo projeto sobre Imagens no Xamarin.Forms

---
+++ Imagens/Parte 2/XF_Imagens2/XF_Imagens2/Extensao.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XF_Imagens2
+{
+    [ContentProperty("Source")]
+    public class Extensao : IMarkupExtension
+    {
+        public string Source { get; set; }
+
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (Source == null)
+            {
+                return null;
+            }
+            var imageSource = ImageSource.FromResource(Source);
+            return imageSource;
+        }
+    }
+}
