b9d3571f8d0d3ee52ae6f60f76ab05a7f1892f87
Paulo <Paulo@DESKTOP-HOTO6IV>
2017-04-30 19:27:15 -0300

    Adição de projeto de iamgens para Xamarin.Forms

---
+++ Imagens/Parte 6/XF_Imagens6/XF_Imagens6/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XF_Imagens6
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            var image = new Image
+            {
+                Source = ImageSource.FromUri(new Uri("http://www.randrade.net/wp-content/uploads/2017/04/lampada.png")),
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.Center
+            };
+
+            Content = image;
+        }
+    }
+}
