957f2a395f6bceb1f3e8e3c9a28003422d4796e4
Paulo <Paulo@DESKTOP-HOTO6IV>
2017-05-28 18:12:25 -0300

    Adição de projeto de ListView para Xamarin.Forms

---
+++ ListViews Avançado/Parte 3/XF_ListViewAdv3/XF_ListViewAdv3/Model.cs
@@ -0,0 +1,13 @@
+﻿namespace XF_ListViewAdv3
+{
+    public class Model
+    {
+        public string Info1 { get; set; }
+
+        public string Info2 { get; set; }
+
+        public string Info3 { get; set; }
+
+        public string Info4 { get; set; }
+    }
+}
