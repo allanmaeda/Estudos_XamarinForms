7e85e92f1ff9394ab9e668f1f27b10fb7d5befdf
Paulo <Paulo@DESKTOP-HOTO6IV>
2017-05-28 17:43:33 -0300

    Adição de projeto sobre ListViews

---
+++ ListViews Avançado/Parte 2/XF_ListViewAdv2/XF_ListViewAdv2/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Xamarin.Forms;
+
+namespace XF_ListViewAdv2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            var lista = new List<Model>();
+
+            for (var i = 0; i < 30; i++)
+            {
+                lista.Add(new Model
+                {
+                    InfoEsq = $"InfoEsq{i+1}",
+                    InfoDir = $"InfoDir{i+1}"
+                });        
+            }
+
+            ListView.ItemsSource = new ObservableCollection<Model>(lista);
+        }
+    }
+}
---
+++ ListViews Avançado/Parte 2/XF_ListViewAdv2/XF_ListViewAdv2/Model.cs
@@ -0,0 +1,9 @@
+﻿namespace XF_ListViewAdv2
+{
+    public class Model
+    {
+        public string InfoEsq { get; set; }
+
+        public string InfoDir { get; set; }
+    }
+}
