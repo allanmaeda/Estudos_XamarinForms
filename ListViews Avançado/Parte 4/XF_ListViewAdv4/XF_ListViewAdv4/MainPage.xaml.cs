6dddb5f9921d05b4d2c6f168013b0e1be3f76fea
Paulo <Paulo@DESKTOP-HOTO6IV>
2017-06-11 14:10:32 -0300

    Adição de projeto de ListView para Xamarin.Forms

---
+++ ListViews Avançado/Parte 4/XF_ListViewAdv4/XF_ListViewAdv4/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XF_ListViewAdv4
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            var listaDeElementos = new List<Model>();
+
+            for (var i = 0; i < 30; i++)
+            {
+                listaDeElementos.Add(new Model
+                {
+                    Texto1 = $"Texto1_{i+1}",
+                    Texto2 = $"Botão2_{i+1}",
+                    Texto3 = $"Texto3_{i+1}",
+                    Texto4 = $"Botão4_{i+1}"
+                });
+            }
+
+            ListView.ItemsSource = new ObservableCollection<Model>(listaDeElementos);
+            
+        }
+    }
+}
---
+++ ListViews Avançado/Parte 4/XF_ListViewAdv4/XF_ListViewAdv4/Model.cs
@@ -0,0 +1,13 @@
+﻿namespace XF_ListViewAdv4
+{
+    public class Model
+    {
+        public string Texto1 { get; set; }
+
+        public string Texto2 { get; set; }
+
+        public string Texto3 { get; set; }
+
+        public string Texto4 { get; set; }
+    }
+}
