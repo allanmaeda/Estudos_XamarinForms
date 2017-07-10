31a8945d725e9f9562fb0daffdbfbb3008d731ff
Paulo <Paulo@DESKTOP-HOTO6IV>
2017-07-09 21:05:51 -0300

    Adição de projeto sobre NavigationPage para Xamarin.Forms

---
+++ Navegacao/Navigation/Parte 8/XF_NavigationPage8/XF_NavigationPage8/MainPage5.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XF_NavigationPage8
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage5 : ContentPage
+    {
+        public MainPage5()
+        {
+            InitializeComponent();
+        }
+
+        private void VoltarParaPagina3(object sender, EventArgs e)
+        {
+            // removendo a pagina 4 da pilha
+            Navigation.RemovePage(Navigation.NavigationStack[3]);
+            Navigation.PopAsync();
+        }
+    }
+}
