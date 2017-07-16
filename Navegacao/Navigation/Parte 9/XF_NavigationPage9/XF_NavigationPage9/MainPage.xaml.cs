e91adf2427a14d5896639234359cde3e7b191a32
Paulo <Paulo@DESKTOP-HOTO6IV>
2017-07-16 17:33:46 -0300

    Adição de projeto sobre NavigationPage para Xamarin.Forms

---
+++ Navegacao/Navigation/Parte 9/XF_NavigationPage9/XF_NavigationPage9/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace XF_NavigationPage9
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_OnClicked(object sender, EventArgs e)
+        {
+            var novaPagina = new MainPage4();
+            await Navigation.PushAsync(novaPagina);
+            Navigation.InsertPageBefore(new MainPage2(), novaPagina);
+            Navigation.InsertPageBefore(new MainPage3(), novaPagina);
+        }
+    }
+}
