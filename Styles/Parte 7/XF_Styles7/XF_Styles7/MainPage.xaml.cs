746d3ffcd01b7c8b5e1cb00bc326523d87e95b9e
Paulo <Paulo@DESKTOP-HOTO6IV>
2017-07-23 13:18:52 -0300

    Adição de projeto sobre styles para Xamarin.Forms

---
+++ Styles/Parte 7/XF_Styles7/XF_Styles7/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace XF_Styles7
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            Resources["Estilo"] = Resources["StyleExemploSemDestaque"];
+        }
+
+
+        private void DarDestaque_OnClick(object sender, EventArgs e)
+        {
+            Resources["Estilo"] = Resources["StyleExemploComDestaque"];
+        }
+
+        private void TirarDestaque_OnClick(object sender, EventArgs e)
+        {
+            Resources["Estilo"] = Resources["StyleExemploSemDestaque"];
+        }
+    }
+}
