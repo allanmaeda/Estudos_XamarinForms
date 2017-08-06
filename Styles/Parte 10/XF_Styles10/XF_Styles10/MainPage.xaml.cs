e9b109b65ff8bb64c850c9ce1d76494df459bafe
Paulo <Paulo@DESKTOP-HOTO6IV>
2017-08-06 09:33:22 -0300

    Adição de projeto sobre Styles para Xamarin.Forms

---
+++ Styles/Parte 10/XF_Styles10/XF_Styles10/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace XF_Styles10
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_OnClicked(object sender, EventArgs e)
+        {
+            var r = new Random();
+            var vl = r.Next(0, 2);
+
+            if (vl == 0)
+                Resources["StyleDinamico"] = Application.Current.Resources["Style1"];
+            else
+                Resources["StyleDinamico"] = Application.Current.Resources["Style2"];
+        }
+    }
+}
