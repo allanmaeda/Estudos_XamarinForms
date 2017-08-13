41a7cb64468389d272556122ddbca6fef367c164
Paulo <Paulo@DESKTOP-HOTO6IV>
2017-08-13 12:35:45 -0300

    Adição de projeto sobre Application para Xamarin.Forms

---
+++ Application/Parte 4/XF_App4/XF_App4/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+using Xamarin.Forms;
+
+namespace XF_App4
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new NavigationPage(new MainPage());
+        }
+
+        protected override void OnStart()
+        {
+
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+
+
+    }
+}
