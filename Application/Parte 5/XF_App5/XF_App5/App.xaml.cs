b002b15e13f54b984f4ecc8b8a730e69bc4fbc56
Paulo <Paulo@DESKTOP-HOTO6IV>
2017-08-13 17:10:27 -0300

    Adição de projeto sobre a classe Application para Xamarin.Forms

---
+++ Application/Parte 5/XF_App5/XF_App5/App.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace XF_App5
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new XF_App5.MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            ModalPopping += async (sender, args) =>
+            {
+                if (new Random().Next(0, 2) == 0)
+                {
+                    args.Cancel = true;
+                    await args.Modal.DisplayAlert("Atenção", "Há algo errado!", "Ok");
+                }
+                else
+                {
+                    await args.Modal.DisplayAlert("Atenção", "Navegação feita!", "Ok");
+                }
+            };
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
