6e9a5526db8c46015a9fc47e6281a3ecf18bb825
Paulo <Paulo@DESKTOP-HOTO6IV>
2017-08-20 16:37:24 -0300

    Adição de projeto sobre TabbedPage para Xamarin.Forms

---
+++ TabbedPage/Parte 4/XF_TabbedPage4/XF_TabbedPage4/Model.cs
@@ -0,0 +1,13 @@
+﻿namespace XF_TabbedPage4
+{
+    public class Model
+    {
+        public string Titulo { get; set; }
+
+        public string Texto1 { get; set; }
+
+        public string Texto2 { get; set; }
+
+        public string Texto3 { get; set; }
+    }
+}
