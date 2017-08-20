0f2624e91a13b456d722a821ba491e3cd40b9811
Paulo <Paulo@DESKTOP-HOTO6IV>
2017-08-20 17:32:22 -0300

    Adição de projeto sobre TabbedPage para Xamarin.Forms

---
+++ TabbedPage/Parte 5/XF_TabbedPage5/XF_TabbedPage5/MainPage2.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XF_TabbedPage5
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage2 : ContentPage
+    {
+        public MainPage2()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
---
+++ TabbedPage/Parte 5/XF_TabbedPage5/XF_TabbedPage5/TabPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XF_TabbedPage5
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TabPage : TabbedPage
+    {
+        public TabPage()
+        {
+            InitializeComponent();
+
+            Children.Add(new MainPage { Title = "P1" });
+            Children.Add(new MainPage2 { Title = "P2" });
+            Children.Add(new MainPage3 { Title = "P3" });
+        }
+    }
+}
