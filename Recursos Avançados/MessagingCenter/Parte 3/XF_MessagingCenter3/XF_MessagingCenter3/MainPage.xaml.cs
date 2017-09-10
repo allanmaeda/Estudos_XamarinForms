6bc4612509434c9cb8e44cde1153d0f4aa0edbb9
Paulo <Paulo@DESKTOP-HOTO6IV>
2017-09-10 17:58:38 -0300

    Adição de projeto sobre MessagingCenter para Xamarin.Forms

---
+++ Recursos Avançados/MessagingCenter/Parte 3/XF_MessagingCenter3/XF_MessagingCenter3/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace XF_MessagingCenter3
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void EnviarMensagem_Clicked(object sender, EventArgs e)
+        {
+            MessagingCenter.Send(this, "msg");
+        }
+
+        private void ReceberMensagem_Clicked(object sender, EventArgs e)
+        {
+            MessagingCenter.Subscribe<MainPage>(this, "msg", async page =>
+            {
+                await DisplayAlert("Atenção!", "Mensagem recebida", "Ok");
+            });
+        }
+
+        private void IgnorarMensagem_Click(object sender, EventArgs e)
+        {
+            MessagingCenter.Unsubscribe<MainPage>(this, "msg");
+        }
+    }
+}
