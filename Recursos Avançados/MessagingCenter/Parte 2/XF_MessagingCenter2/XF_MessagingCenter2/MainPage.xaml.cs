8ec648110022b6535e8aabb7642873b8cfe175b6
Paulo <Paulo@DESKTOP-HOTO6IV>
2017-09-10 15:11:25 -0300

    Adição de projeto sobre MessagingCenter para Xamarin.Forms

---
+++ Recursos Avançados/MessagingCenter/Parte 2/XF_MessagingCenter2/XF_MessagingCenter2/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace XF_MessagingCenter2
+{
+    public partial class MainPage : ContentPage
+    {
+        public DateTime DataDeEnvioDaMensagem { get; private set; }
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void EnviarMensagem_Clicked(object sender, EventArgs e)
+        {
+            DataDeEnvioDaMensagem = DateTime.Now;
+            MessagingCenter.Send(this, "msg");
+        }
+
+        private void ReceberMensagem_Clicked(object sender, EventArgs e)
+        {
+            MessagingCenter.Subscribe<MainPage>(this, "msg", async page =>
+            {
+                await DisplayAlert("Atenção", $"mensagem recebida às {page.DataDeEnvioDaMensagem:HH:mm:ss}", "Ok");
+            });
+        }
+    }
+}
