73afd494c7e08a3abba12e3f9132d67fa5bae765
Paulo <Paulo@DESKTOP-HOTO6IV>
2017-09-10 13:31:01 -0300

    Adição de código sobre MessagingCenter para Xamarin.Forms

---
+++ Recursos Avançados/MessagingCenter/XF_MessagingCenter1/XF_MessagingCenter1/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace XF_MessagingCenter1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void EnviarMensagem_Clicked(object sender, EventArgs e)
+        {
+            MessagingCenter.Send(this, "msg");
+        }
+
+        private void ReceberMensagem_Clicked(object sender, EventArgs e)
+        {
+            MessagingCenter.Subscribe<MainPage>(this, "msg", async page =>
+            {
+                await DisplayAlert("Atenção", "mensagem recebida", "Ok");
+            });
+        }
+    }
+}
