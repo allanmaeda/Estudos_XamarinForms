eee9efd999be8651f129124428885e6ac1378806
Paulo <Paulo@DESKTOP-HOTO6IV>
2017-09-24 10:21:39 -0300

    Adição de projeto sobre MessagingCenter para Xamarin.Forms

---
+++ Recursos Avançados/MessagingCenter/Parte 7/XF_MessagingCenter7/XF_MessagingCenter7/Page1.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XF_MessagingCenter7
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+            MessagingCenter.Subscribe<Page2, string>(this, "msg", (page2, s) => SegundaEntry.Text = s);
+            MessagingCenter.Subscribe<Page3, string>(this, "msg", (page3, s) => TerceiraEntry.Text = s);
+        }
+
+        private async void Button_OnClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Page2());
+        }
+    }
+}
---
+++ Recursos Avançados/MessagingCenter/Parte 7/XF_MessagingCenter7/XF_MessagingCenter7/Page2.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XF_MessagingCenter7
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page2 : ContentPage
+    {
+        public Page2()
+        {
+            InitializeComponent();
+        }
+
+        private void Entry_OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            MessagingCenter.Send(this, "msg", e.NewTextValue);
+        }
+
+        private async void Button_OnClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Page3());
+        }
+    }
+}
