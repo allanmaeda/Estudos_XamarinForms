1f632611c6afeab759b8917d57dead59eb23b593
Paulo <Paulo@DESKTOP-HOTO6IV>
2017-10-14 19:55:21 -0300

    Organização de código

---
+++ Navegacao/MasterDetail/Parte 1/XF_MasterDetail1/XF_MasterDetail1/Views/Detalhe.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XF_MasterDetail1.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Detalhe : ContentPage
+    {
+        public Detalhe()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Navegacao/MasterDetail/Parte 1/XF_MasterDetail1/XF_MasterDetail1/Views/Menu.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using XF_MasterDetail1.Models;
+
+namespace XF_MasterDetail1.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Menu : ContentPage
+    {
+        public Menu()
+        {
+            InitializeComponent();
+
+            ListView.ItemsSource = new List<ItemMenu>
+            {
+                new ItemMenu {Texto = "Opção 1"},
+                new ItemMenu { Texto = "Opção 2"},
+                new ItemMenu { Texto = "Opção 3"},
+            };
+        }
+    }
+}
