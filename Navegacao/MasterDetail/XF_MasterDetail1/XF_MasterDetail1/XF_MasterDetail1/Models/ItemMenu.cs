05dacfcdff6396fec9b97b8065d625d6ec738ef9
Paulo <Paulo@DESKTOP-HOTO6IV>
2017-10-14 19:49:50 -0300

    Adição de projeto sobre MasterDetailPage para Xamarin.Forms

---
+++ Navegacao/MasterDetail/XF_MasterDetail1/XF_MasterDetail1/XF_MasterDetail1/Models/ItemMenu.cs
@@ -0,0 +1,7 @@
+﻿namespace XF_MasterDetail1.Models
+{
+    public class ItemMenu
+    {
+        public string Texto { get; set; }
+    }
+}
---
+++ Navegacao/MasterDetail/XF_MasterDetail1/XF_MasterDetail1/XF_MasterDetail1/Views/MasterDetail.cs
@@ -0,0 +1,13 @@
+﻿using Xamarin.Forms;
+
+namespace XF_MasterDetail1.Views
+{
+    public class MasterDetail : MasterDetailPage
+    {
+        public MasterDetail()
+        {
+            Master = new Menu();
+            Detail = new NavigationPage(new Detalhe());
+        }
+    }
+}
