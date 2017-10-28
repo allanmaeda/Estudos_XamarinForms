362e2b65d596196bf3fd70f522c249902eed8f46
Paulo <Paulo@DESKTOP-HOTO6IV>
2017-10-28 09:14:20 -0200

    Adição de projeto sobre MasterDetailPage para Xamarin.Forms

---
+++ Navegacao/MasterDetail/Parte 9/XF_MasterDetail9/XF_MasterDetail9/Models/ItemMenu.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace XF_MasterDetail9.Models
+{
+    public class ItemMenu
+    {
+        public string Texto { get; set; }
+
+        public Type Type { get; set; }
+    }
+}
---
+++ Navegacao/MasterDetail/Parte 9/XF_MasterDetail9/XF_MasterDetail9/Views/MasterDetail.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Xamarin.Forms;
+using XF_MasterDetail9.Models;
+
+namespace XF_MasterDetail9.Views
+{
+    public class MasterDetail : MasterDetailPage
+    {
+        public MasterDetail()
+        {
+            Master = new Menu();
+            Detail = new NavigationPage(new Detalhe1());
+
+            ((Menu) Master).PublicListView.ItemSelected += (sender, args) =>
+            {
+                if (!(args.SelectedItem is ItemMenu item))
+                    return;
+
+                ((Menu) Master).PublicListView.SelectedItem = null;
+                Detail = new NavigationPage((Page) Activator.CreateInstance(item.Type));
+
+                if (MasterBehavior == MasterBehavior.Popover)
+                    IsPresented = false;
+            };
+
+            SizeChanged += (sender, args) =>
+            {
+                if (Width > Height)
+                {
+                    IsPresented = true;
+                    MasterBehavior = MasterBehavior.Split;
+                }
+                else
+                {
+                    MasterBehavior = MasterBehavior.Popover;
+                    ((IMasterDetailPageController)this).CanChangeIsPresented = true;
+                    IsPresented = true;
+                }
+            };
+        }
+    }
+}
