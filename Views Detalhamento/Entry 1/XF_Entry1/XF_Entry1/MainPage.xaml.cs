520bd9e032a59f2cb924a8e838fd7deef52799fa
Paulo <Paulo@DESKTOP-HOTO6IV>
2017-10-28 10:03:33 -0200

    Adição de projeto sobre Entry para Xamarin.Forms

---
+++ Views Detalhamento/Entry 1/XF_Entry1/XF_Entry1/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace XF_Entry1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_OnClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new MainPage2(), true);
+        }
+    }
+}
