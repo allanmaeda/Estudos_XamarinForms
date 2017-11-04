248b2402fb771a3609de8c1817fe51124801f067
Paulo <Paulo@DESKTOP-HOTO6IV>
2017-11-04 11:10:32 -0200

    Adição de projeto sobre CarouselPage para Xamarin.Forms

---
+++ Navegacao/Carousel/Parte 1/XF_CarouselPage1/XF_CarouselPage1/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace XF_CarouselPage1
+{
+    public partial class MainPage : CarouselPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Navegacao/Carousel/Parte 2/XF_CarouselPage2/XF_CarouselPage2/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Xamarin.Forms;
+
+namespace XF_CarouselPage2
+{
+    public partial class MainPage : CarouselPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            Children.Add(new Page1());
+            Children.Add(new Page2());
+            Children.Add(new Page3());
+        }
+    }
+}
