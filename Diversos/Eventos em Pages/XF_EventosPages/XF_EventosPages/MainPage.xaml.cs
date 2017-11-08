3556105172a6c3f89afe1130dbbab76de618a087
Paulo <Paulo@DESKTOP-HOTO6IV>
2017-11-08 20:31:25 -0200

    Adição de projeto sobre Eventos de Pages para Xamarin.Forms

---
+++ Diversos/Eventos em Pages/XF_EventosPages/XF_EventosPages/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace XF_EventosPages
+{
+    public partial class MainPage : ContentPage
+    {
+        private MainPage2 Page { get; set; }
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            MessagingCenter.Subscribe<MainPage2>(this, "Constructor", page2 =>
+            {
+                Editor.Text += "Constructor foi chamado" + Environment.NewLine;
+            });
+
+            MessagingCenter.Subscribe<MainPage2>(this, "OnAppearing", page2 =>
+            {
+                Editor.Text += "OnAppearing foi chamado" + Environment.NewLine;
+            });
+
+            MessagingCenter.Subscribe<MainPage2>(this, "OnDisappearing", page2 =>
+            {
+                Editor.Text += "OnDisappearing foi chamado" + Environment.NewLine;
+            });
+
+            MessagingCenter.Subscribe<MainPage2>(this, "OnBackButtonPressed", page2 =>
+            {
+                Editor.Text += "OnBackButtonPressed foi chamado" + Environment.NewLine;
+            });
+        }
+
+        private async void Button_OnClicked(object sender, EventArgs e)
+        {
+            if (Page == null)
+                Page = new MainPage2();
+            await Navigation.PushModalAsync(Page);
+        }
+    }
+}
---
+++ Diversos/Eventos em Pages/XF_EventosPages/XF_EventosPages/MainPage2.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XF_EventosPages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage2 : ContentPage
+    {
+        public MainPage2()
+        {
+            InitializeComponent();
+            MessagingCenter.Send(this, "Constructor");
+            Editor.Text += "Constructor foi chamado" + Environment.NewLine;
+        }
+
+        protected override void OnAppearing()
+        {
+            MessagingCenter.Send(this, "OnAppearing");
+            base.OnAppearing();
+            Editor.Text += "OnAppearing foi chamado" + Environment.NewLine;
+        }
+
+        protected override void OnDisappearing()
+        {
+            MessagingCenter.Send(this, "OnDisappearing");
+            base.OnDisappearing();
+            Editor.Text += "OnDisappearing foi chamado" + Environment.NewLine;
+        }
+
+        protected override bool OnBackButtonPressed()
+        {
+            MessagingCenter.Send(this, "OnBackButtonPressed");
+            Editor.Text += "OnBackButtonPressed foi chamado" + Environment.NewLine;
+            return base.OnBackButtonPressed();
+        }
+
+        private async void Button_OnClicked(object sender, EventArgs e)
+        {
+            await Navigation.PopModalAsync(true);
+        }
+    }
+}
