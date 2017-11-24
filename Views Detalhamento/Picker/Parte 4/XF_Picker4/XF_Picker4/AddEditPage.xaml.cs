aa7c23180b916f4a913ca08135b6528c8b5e1b26
Paulo <Paulo@DESKTOP-HOTO6IV>
2017-11-24 21:53:44 -0200

    Adição de projeto sobre Picker para Xamarin.Forms

---
+++ Views Detalhamento/Picker/Parte 4/XF_Picker4/XF_Picker4/AddEditPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XF_Picker4
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AddEditPage : ContentPage
+    {
+        private Elemento _el;
+
+        public AddEditPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void ButtonCancelar_OnClicked(object sender, EventArgs e)
+        {
+            await Navigation.PopModalAsync(true);
+        }
+
+        private async void ButtonConfirmar_OnClicked(object sender, EventArgs e)
+        {
+            _el = new Elemento
+            {
+                ValorReal = Convert.ToInt32(EntryReal.Text),
+                ValorVisual = EntryVisual.Text
+            };
+            MessagingCenter.Send(this, "NovoElemento", _el);
+            await Navigation.PopModalAsync(true);
+        }
+    }
+}
---
+++ Views Detalhamento/Picker/Parte 4/XF_Picker4/XF_Picker4/Elemento.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel;
+
+namespace XF_Picker4
+{
+    public class Elemento : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string _valorVisual;
+        private int _valorReal;
+
+        public string ValorVisual
+        {
+            get => _valorVisual;
+            set
+            {
+                _valorVisual = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ValorVisual)));
+            }
+        }
+
+        public int ValorReal
+        {
+            get => _valorReal;
+            set
+            {
+                _valorReal = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ValorReal)));
+            }
+        }
+    }
+}
