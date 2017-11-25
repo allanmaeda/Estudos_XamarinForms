39329a88cd8b8dc5f242f2a1cc57d17615282290
Paulo <Paulo@DESKTOP-HOTO6IV>
2017-11-25 17:06:37 -0200

    Adição de projeto sobre Picker para Xamarin.Forms

---
+++ Views Detalhamento/Picker/Parte 5/XF_Picker5/XF_Picker5/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace XF_Picker5
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            var vm = new ViewModel
+            {
+                Elementos = new List<Elemento>
+                {
+                    new Elemento {ValorReal = 123, ValorVisual = "Elemento 1"},
+                    new Elemento {ValorReal = 456, ValorVisual = "Elemento 2"},
+                    new Elemento {ValorReal = 789, ValorVisual = "Elemento 3"}
+                }
+            };
+            
+            BindingContext = vm;
+        }
+    }
+}
