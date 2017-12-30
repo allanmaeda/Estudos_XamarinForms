39e7b801c886d2e3307f18a0e5551eb86e9579e1
Paulo <Paulo@DESKTOP-HOTO6IV>
2017-12-30 19:02:34 -0200

    Adição de projeto sobre a classe Application para Xamarin.Forms

---
+++ Recursos Avançados/Classe Application/Parte 2/XF_App2/XF_App2/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XF_App2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Label1.Text = ((App) Application.Current).ExemploDeValor;
+            Label2.Text = ((App) Application.Current).OutroExemploDeValor;
+
+        }
+    }
+}
---
+++ Recursos Avançados/Classe Application/Parte 3/XF_App3/XF_App3/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace XF_App3
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_OnClicked(object sender, EventArgs e)
+        {
+            if (Application.Current.Properties.ContainsKey("OnStart"))
+                LabelOnStart.Text = $"OnStart em {Application.Current.Properties["OnStart"]:dd-MM-yyyy HH:mm:ss}";
+
+            if (Application.Current.Properties.ContainsKey("OnSleep"))
+                LabelOnSleep.Text = $"OnSleep em {Application.Current.Properties["OnSleep"]:dd-MM-yyyy HH:mm:ss}";
+
+            if (Application.Current.Properties.ContainsKey("OnResume"))
+                LabelOnResume.Text = $"OnResume em {Application.Current.Properties["OnResume"]:dd-MM-yyyy HH:mm:ss}";
+        }
+    }
+}
